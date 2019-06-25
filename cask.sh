e65fa6b4ef2c770614aea76fbd92698327491763
John Kennedy Bicbic <jkbicbic@gmail.com>
2019-06-26 07:25:44 +0800

    added android file transfer app to be installed

--- cask.sh
+++ cask.sh
@@ -5,7 +5,8 @@ brew upgrade
 # install casks
 brew cask install slack
 brew cask install sequel-pro
-brew cask install google-chrome
+brew cask install google-chromea
+brew cask install android-file-transfer
 brew cask transmission
 
 brew cleanup
